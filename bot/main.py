1b10d9271d56bb508946f3b7d700b020ebf5d186
AksaKing <anandarya287@gmail.com>
2026-03-22 13:18:40 +0800

    fix: manually sync agent state with on-chain bounty ID after network timeout

--- bot/main.py
+++ bot/main.py
@@ -31,7 +31,7 @@ class PoidhAutonomousAgent:
         )
         self.model_name = os.getenv("AI_MODEL_NAME", "gpt-4o")
         
-        self.active_bounty_id = 111
+        self.active_bounty_id = 1097
 
     def create_real_world_bounty(self):
         """Creates an on-chain bounty. Fully autonomous execution."""
@@ -83,7 +83,7 @@ class PoidhAutonomousAgent:
         
         try:
             claim_events = self.poidh_contract.events.ClaimCreated().get_logs(
-                from_block=12000000, 
+                from_block=43683329, 
                 argument_filters={'bountyId': self.active_bounty_id}
             )
             
